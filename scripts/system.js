505b6edab15b653e1214d02db0b2ca20d02e5178
Bozar <5583771+Bozar@users.noreply.github.com>
2018-06-09 18:35:56 +0800

    Clean up formats in system.js.

--- scripts/system.js
+++ scripts/system.js
@@ -10,16 +10,22 @@ Main.system.isFloor = function (x, y) {
 Main.system.placePC = function () {
     let x = null;
     let y = null;
-    let width = Main.getEntity('dungeon').Dungeon.getWidth();
-    let height = Main.getEntity('dungeon').Dungeon.getHeight();
-    let border = Main.getEntity('pc').Position.getRange();
 
     do {
-        x = Math.floor(width * ROT.RNG.getUniform());
-        y = Math.floor(height * ROT.RNG.getUniform());
-    } while (!Main.system.isFloor(x, y)
-    || x < border || x > width - border
-    || y < border || y > height - border);
+        x = Math.floor(
+            Main.getEntity('dungeon').Dungeon.getWidth()
+            * ROT.RNG.getUniform());
+        y = Math.floor(
+            Main.getEntity('dungeon').Dungeon.getHeight()
+            * ROT.RNG.getUniform());
+    } while (
+        !Main.system.isFloor(x, y)
+        || x < Main.getEntity('pc').Position.getRange()
+        || x > Main.getEntity('dungeon').Dungeon.getWidth()
+        - Main.getEntity('pc').Position.getRange()
+        || y < Main.getEntity('pc').Position.getRange()
+        || y > Main.getEntity('dungeon').Dungeon.getHeight()
+        - Main.getEntity('pc').Position.getRange());
 
     Main.getEntity('pc').Position.setX(x);
     Main.getEntity('pc').Position.setY(y);
@@ -41,10 +47,7 @@ Main.system.isInSight = function (source, targetX, targetY) {
         source.Position.getX(),
         source.Position.getY(),
         source.Position.getRange(),
-        (x, y) => {
-            sight.push(x + ',' + y);
-        }
-    );
+        (x, y) => { sight.push(x + ',' + y); });
 
     return sight.indexOf(targetX + ',' + targetY) > -1;
 };
